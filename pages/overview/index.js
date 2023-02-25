cdc86b939e899aee5daf9ad9aa9f57642d82e612
Abdelalim03 <ka_atoui@esi.dz>
2023-02-25 05:12:18 +0100

    99% done

--- pages/overview/index.js
+++ pages/overview/index.js
@@ -1,11 +1,17 @@
 import {React, useEffect, useRef, useState} from 'react';
-import {text} from './text';
 import { motion, useAnimation, useInView} from 'framer-motion';
 
 
 
 function Overview() {
-
+     const text = [
+        {p1 : 'It is safe to say that Modern cryptography is currently unbreakable and that is primary due to the complexity of the math behind it, the length of keys used, and we can add to that the incapability of current computers to break large ciphers.',
+         p2 : 'But that era of safe data is actually being threatened by the appearance of QCs powerful devices based on new technologies.',
+         p3 : 'Current QCs cant break our standard encryption method, but predictions assure that this may change in an early future',
+         p4 : 'It is therefore crucial to be able to find and implement post quantum methods, not for now, but for an upcoming future!',
+         p5 : 'This is where QTM comes in, by providing and implementation of the NTRUEcrypt algorithm, which can’t be broken by QCs in a 100 years.'
+    },
+    ];
     const controls = useAnimation();
     const ref = useRef(null)
     const isInView = useInView(ref)
